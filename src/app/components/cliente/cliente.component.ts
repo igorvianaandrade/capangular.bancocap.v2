b024e9e28f3fb2b54b87d01ddff21349c9fb4c83
igorvianaandrade <vianaigo@gmail.com>
2020-12-12 08:45:30 -0300

    Add  ngx-mask

--- src/app/components/cliente/cliente.component.ts
+++ src/app/components/cliente/cliente.component.ts
@@ -23,13 +23,4 @@ export class ClienteComponent implements OnInit {
     alert("Cliente criado com sucesso!");
     console.log(this.cliente);
   }
-
-  isCPF(): boolean{
-    return this.cliente.cpf == null ? true : this.cliente.cpf.length < 12 ? true : false;
- }
-
-  getCpfCnpjMask(): string{
-    return this.isCPF() ? '000.000.000-009' : '00.000.000/0000-00';
- }
-
 }
